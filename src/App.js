60cfa54075a3cd8257fc1725c34c6f6dccc33321
Mohammed Nabeel P <111896737+mdnabeelMCA23@users.noreply.github.com>
2025-11-29 22:29:48 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -215,7 +215,7 @@ function App() {
             </div>
             <div style={logoTextStyle}>
               <h1 style={darkMode ? darkTitleStyle : lightTitleStyle}>
-                {isMobile ? 'MN AI' : 'MN AI Assistant'}
+                {isMobile ? 'Ayisha AI' : 'Ayisha AI Assistant'}
               </h1>
               {!isMobile && (
                 <p style={darkMode ? darkSubtitleStyle : lightSubtitleStyle}>
